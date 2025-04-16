84d8c730caa48c9d3aa196d696f12b0b84d4aa5c
leecool-777 <siwon0726@naver.com>
2025-04-16 19:28:37 +0900

    예외처리 추가

--- src/lv1/App.java
+++ src/lv1/App.java
@@ -1,5 +1,6 @@
 package lv1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -11,49 +12,57 @@ public class App {
 
 
         while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int num1 = scanner.nextInt();
+            try {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                int num1 = scanner.nextInt();
 
-            if (num1 < 0) {
-                System.out.println("양의 정수만 입력 가능합니다 !");
-                continue;
-            }
-
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int num2 = scanner.nextInt();
-            if (num2 < 0) {
-                System.out.println("양의 정수만 입력 가능합니다 !");
-                continue;
-            }
-
-            System.out.print("사칙연산 기호를 입력하세요(+ - * /): ");
-            char cal = scanner.next().charAt(0);
-            scanner.nextLine();
-
-            int result = 0;
-            switch (cal) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("0으로 나눌 수 없습니다.");
-                        continue;
-                    }
-                    result = num1 / num2;
-                    break;
-                default:
-                    System.out.println("잘못된 값입니다. 다시 입력해주세요 !");
+                if (num1 < 0) {
+                    System.out.println("양의 정수만 입력 가능합니다 !");
                     continue;
-            }
+                }
 
-            System.out.println("결과: " + result);
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                int num2 = scanner.nextInt();
+                if (num2 < 0) {
+                    System.out.println("양의 정수만 입력 가능합니다 !");
+                    continue;
+                }
+
+                System.out.print("사칙연산 기호를 입력하세요(+ - * /): ");
+                char cal = scanner.next().charAt(0);
+                scanner.nextLine(); // Scanner 버퍼 처리
+
+                //사칙연산 수행
+                int result = 0;
+                switch (cal) {
+                    case '+':
+                        result = num1 + num2;
+                        break;
+                    case '-':
+                        result = num1 - num2;
+                        break;
+                    case '*':
+                        result = num1 * num2;
+                        break;
+                    case '/':
+                        if (num2 == 0) {
+                            System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                            continue;
+                        }
+                        result = num1 / num2;
+                        break;
+                    default:
+                        System.out.println("잘못된 값입니다. 다시 입력해주세요 !");
+                        continue;
+                }
+                System.out.println("결과: " + result);
+
+                //예외처리
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 값입니다. 숫자를 입력해주세요");
+                scanner.nextLine(); //Scanner 버퍼 정리
+                continue;
+            }
 
             System.out.print("더 계산하시겠습니까? (exit 입력시 종료, 계산을 원하시면 엔터를 눌러주세요 !) : ");
             String option = scanner.nextLine();
