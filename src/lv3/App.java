1503491a41c5cd8b391d542bd73d9c1b0aaad532
leecool-777 <siwon0726@naver.com>
2025-04-17 16:52:30 +0900

    lv3 메인메서드 변수 이름 수정

--- src/lv3/App.java
+++ src/lv3/App.java
@@ -1,7 +1,5 @@
 package lv3;
 
-import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -15,22 +13,22 @@ public class App {
         System.out.println("===========================");
 
         while (true) {
-            Number a;
-            Number b;
+            Number num1;
+            Number num2;
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            String num1 = scanner.nextLine();
-            if (num1.contains(".")) {
-                 a = Double.parseDouble(num1);
+            String inputNum1 = scanner.nextLine();
+            if (inputNum1.contains(".")) {
+                 num1 = Double.parseDouble(inputNum1);
             } else {
-                 a = Integer.parseInt(num1);
+                 num1 = Integer.parseInt(inputNum1);
             }
 
             System.out.print("두 번째 숫자를 입력하세요: ");
-            String num2 = scanner.nextLine();
-            if (num2.contains(".")) {
-                 b = Double.parseDouble(num2);
+            String inputNum2 = scanner.nextLine();
+            if (inputNum2.contains(".")) {
+                 num2 = Double.parseDouble(inputNum2);
             } else {
-                 b = Integer.parseInt(num2);
+                 num2 = Integer.parseInt(inputNum2);
             }
             System.out.print("사칙연산 기호를 입력하세요(+ - * /): ");
             char operator = scanner.next().charAt(0);
@@ -41,7 +39,7 @@ public class App {
 
             //입력값 전달 및 연산 결과 반환
             System.out.println("===========================");
-            Number result = arithmeticCalculator.calculate(a, b, calOperator);
+            Number result = arithmeticCalculator.calculate(num1, num2, calOperator);
             System.out.println("결과: " + result);
             System.out.println("결과 데이터: " + arithmeticCalculator.getResults());
             System.out.println("===========================");
